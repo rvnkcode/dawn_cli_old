b27b813632eebb04e3794d7acf849445ebbff054
Kim Soyeon <kim.soyeon@plus-zero.co.jp>
2023-10-30 18:14:13 +0900

    Create sqlite .db file and todo table if not exists

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+use rusqlite::Connection;
 use std::fs::create_dir;
 
 fn main() {
     let path = dirs::home_dir().unwrap().join(".dawn");
     /*
-    // Debug
-    println!("{:}", path.display());
+    println!("{:}", path.display()); // Debug
     */
 
     if path.exists() {
-        println!("...Path checked");
+        println!("...Directory checked");
     } else {
-        create_dir(&path).expect("Fail");
-        println!("...Path created");
-        // TODO: Create sqlite .db file
+        create_dir(&path).expect("Error");
+        println!("...Directory created");
     }
+
+    // Create sqlite .db file and todo table
+    let path = path.join("todo.db");
+    let conn = Connection::open(&path).expect("Error");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+                id      INTEGER PRIMARY KEY,
+                name    TEXT NOT NULL
+            )",
+        (),
+    ).expect("Error");
 }
