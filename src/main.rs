6ab6c97fc1bf5f2545a917f08837dc100d5daa32
Kim Soyeon <kim.soyeon@plus-zero.co.jp>
2023-10-30 17:18:48 +0900

    Create the directory if it doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::create_dir;
+
 fn main() {
-    println!("Hello, world!");
+    let path = dirs::home_dir().unwrap().join(".dawn");
+    /*
+    // Debug
+    println!("{:}", path.display());
+    */
+
+    if path.exists() {
+        println!("...Path checked");
+    } else {
+        create_dir(&path).expect("Fail");
+        println!("...Path created");
+        // TODO: Create sqlite .db file
+    }
 }
