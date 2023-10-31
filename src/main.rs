1354adcaa271c9ec8f0353a16d25c862506c1b45
rvnkcode <sykim.p508@gmail.com>
2023-10-31 20:03:41 +0900

    Check DB exists and proceed seeding

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ struct Todo {
 }
 
 fn main() {
-    let path = define_path();
+    let path = define_directory();
     // println!("{:}", path.display()); // Debug
     check_directory(&path);
 
     let path = path.join("todo.db");
-    initialize_db(&path);
+    check_db(&path);
 }
 
-fn define_path() -> PathBuf {
+fn define_directory() -> PathBuf {
     let path = match dirs::home_dir() {
         Some(p) => p,
         None => PathBuf::new(),
@@ -31,14 +31,34 @@ fn check_directory(path: &PathBuf) {
     if path.exists() {
         println!("...Directory checked");
     } else {
-        create_dir(&path).expect("Error");
+        create_dir(&path).expect("Directory creation failed");
         println!("...Directory created");
     }
 }
 
-fn initialize_db(path: &PathBuf) {
-    let conn = Connection::open(&path).expect("Error");
+fn check_db(path: &PathBuf) {
+    if path.exists() {
+        println!("...DB checked");
+    } else {
+        let conn = Connection::open(&path).expect("Connection open failed");
+        initialize_db(&conn);
+        seeding(&conn);
+    }
+}
+
+fn initialize_db(conn: &Connection) {
     conn.execute(include_str!("./sql/schema.sql"), ())
         .expect("Error");
     println!("...DB initialized");
+}
+
+fn seeding(conn: &Connection) {
+    let count: u32 = conn
+        .query_row("SELECT COUNT(*) FROM todo", [], |row| row.get(0))
+        .expect("To-do count query failed");
+    if count < 1 {
+        conn.execute(include_str!("./sql/seed.sql"), ())
+            .expect("Seeding failed");
+        println!("...Seeding completed");
+    }
 }
