0ff8c944979a09462a0417f3e99445fdb5f69cc4
Kim Soyeon <kim.soyeon@plus-zero.co.jp>
2023-11-02 10:58:41 +0900

    Display message after create a new To-Do

--- src/db.rs
+++ src/db.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 
 pub fn check_db(path: &PathBuf) {
     if !path.exists() {
-        let conn = Connection::open(&path).expect("Connection open failed");
+        let conn = Connection::open(&path).unwrap();
         initialize_db(&conn);
         seeding(&conn);
     }
@@ -19,7 +19,7 @@ pub fn initialize_db(conn: &Connection) {
 pub fn seeding(conn: &Connection) {
     let count: u32 = conn
         .query_row("SELECT COUNT(*) FROM todo", [], |row| row.get(0))
-        .expect("To-do count query failed");
+        .unwrap();
     if count < 1 {
         conn.execute(include_str!("./sql/seed.sql"), ())
             .expect("Seeding failed");
@@ -28,10 +28,19 @@ pub fn seeding(conn: &Connection) {
 }
 
 pub fn create_todo(todo: &AddArgs, path: &PathBuf) {
-    let conn = Connection::open(path).expect("Connection open failed");
+    let conn = Connection::open(&path).unwrap();
     conn.execute(
         "INSERT INTO todo (title, is_completed) VALUES (?1, ?2)",
         (&todo.title, &todo.check),
     )
     .expect("Failed to create To-Do");
+
+    match conn.query_row(
+        "SELECT id FROM todo WHERE title=(?1) ORDER BY updated_at DESC LIMIT 1",
+        [&todo.title],
+        |row| row.get::<_, u32>(0),
+    ) {
+        Ok(result) => println!("Created task {:?}", result),
+        Err(e) => panic!("Failed to create a new To-Do: {:?}", e),
+    };
 }
