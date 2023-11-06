d1de51b89d73de010e9307bbd810c1772466bde8
rvnkcode <sykim.p508@gmail.com>
2023-11-06 21:07:42 +0900

    Update seed: Add deleted to-do sample

--- src/sql/seed.sql
+++ src/sql/seed.sql
@@ -1,5 +1,6 @@
 INSERT INTO
-  todo (title, is_completed, completed_at)
+  todo (title, is_completed, completed_at, is_deleted)
 VALUES
-  ("This is the sample To-Do", 0, NULL),
-  ("Completed To-Do sample", 1, CURRENT_TIMESTAMP);
+  ("This is the sample To-Do", 0, NULL, 0),
+  ("Completed To-Do sample", 1, CURRENT_TIMESTAMP, 0),
+  ("Deleted", 0, NULL, 1);
