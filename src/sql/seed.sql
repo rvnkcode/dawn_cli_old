6644d5eef9fefede307cfc6e72ee616e644fad8f
rvnkcode <sykim.p508@gmail.com>
2023-11-07 19:13:35 +0900

    Update seed: Add some notes

--- src/sql/seed.sql
+++ src/sql/seed.sql
@@ -2,6 +2,6 @@ INSERT INTO
   todo (title, note, is_completed, completed_at, is_deleted)
 VALUES
   ("This is the sample To-Do", "Note test", 0, NULL, 0),
-  ("Completed To-Do sample", NULL, 1, CURRENT_TIMESTAMP, 0),
-  ("Deleted", NULL, 0, NULL, 1),
+  ("Completed To-Do sample", "Note test", 1, CURRENT_TIMESTAMP, 0),
+  ("Deleted", "Note test", 0, NULL, 1),
   ("Deleted and completed", NULL, 1, CURRENT_TIMESTAMP, 1);
